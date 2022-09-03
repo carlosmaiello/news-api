460ff153cb96ff53b5f9184a571bf240ffc00ba6
Carlos Maiello <carlosmaiello@gmail.com>
2022-09-02 22:29:34 -0300

    Relacionamento

--- src/models.js
+++ src/models.js
@@ -37,9 +37,59 @@ const News = database.define('news', {
     }
 });
 
-// const User = database.define('user', {
-//  ...
-// });
+const Category = database.define('category', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+        validate: {
+            notEmpty: true
+        }
+    },
+})
+
+const User = database.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            notEmpty: true,
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
+    },
+});
+
+User.hasMany(News, {
+    foreignKey: {
+        allowNull: false
+    }
+});
+News.belongsTo(User);
 
 
-module.exports = { database, News }
+module.exports = { database, News, Category, User }
--- src/models.test.js
+++ src/models.test.js
@@ -1,138 +1,235 @@
-const { database, News } = require('./models');
+const { database, News, Category, User } = require('./models');
 
-describe("News", () => {
+
+describe("Models", () => {
+
+    var user;
 
     beforeAll(async () => {
-        await database.sync();
+        await database.sync(); 
+        user = await User.create({
+            name: "Zezinho",
+            email: "ze@uol.com.br",
+            password: "123"
+        })
     });
 
-    test("List", async () => {
-        await News.bulkCreate([
-            {
+    describe("News", () => {
+
+        test("List", async () => {
+            await News.bulkCreate([
+                {
+                    userId: user.id,
+                    title: "News 01",
+                    subject: "News Subject 01",
+                    content: "News Content 01",
+                    image: "https://picsum.photos/1920/350?random=1",
+                    publishedAt: "2022-08-31T08:00:00"
+                },
+                {
+                    userId: user.id,
+                    title: "News 02",
+                    subject: "News Subject 02",
+                    content: "News Content 02",
+                    image: "https://picsum.photos/1920/350?random=1",
+                    publishedAt: "2022-08-31T08:00:00"
+                },
+                {
+                    userId: user.id,
+                    title: "News 03",
+                    subject: "News Subject 03",
+                    content: "News Content 03",
+                    image: "https://picsum.photos/1920/350?random=1",
+                    publishedAt: "2022-08-31T08:00:00"
+                }
+            ]);
+
+            const news = await News.findAll();
+
+            expect(news.length).toBe(3);
+            expect(news[0].title).toBe("News 01");
+        });
+
+        test("Insert", async () => {
+            // Inserindo com todos os dados      
+            const news = await News.create({
+                userId: user.id,
                 title: "News 01",
                 subject: "News Subject 01",
                 content: "News Content 01",
                 image: "https://picsum.photos/1920/350?random=1",
                 publishedAt: "2022-08-31T08:00:00"
-            },
-            {
-                title: "News 02",
-                subject: "News Subject 02",
-                content: "News Content 02",
+            });
+
+            expect(news).toBeDefined();
+            expect(news).not.toBeNull();
+            expect(news.id).toBeDefined();
+            expect(news.title).toBe("News 01");
+
+            // Inserindo com o mínimo de dados
+            await expect(News.create({
+                userId: user.id,
+                title: "News 01",
+                subject: "News Subject 01",
+                content: "News Content 01",
+            })).resolves.toBeDefined();
+
+            // Inserindo com os dados brancos
+            await expect(News.create({
+                userId: user.id,
+                title: "",
+                subject: "",
+                content: "",
+            })).rejects.toThrow();
+
+            await expect(News.create({
+                userId: user.id,
+                title: "",
+                subject: "xxx",
+                content: "xxx",
+            })).rejects.toThrow();
+
+            await expect(News.create({
+                userId: user.id,
+                title: "xxx",
+                subject: "",
+                content: "xxx",
+            })).rejects.toThrow();
+
+            await expect(News.create({
+                userId: user.id,
+                title: "xxx",
+                subject: "xxx",
+                content: "",
+            })).rejects.toThrow();
+
+            // Inserindo sem data de publicação
+            await expect(News.create({
+                userId: user.id,
+                title: "News 01",
+                subject: "News Subject 01",
+                content: "News Content 01",
+                image: "https://picsum.photos/1920/350?random=1",
+            })).resolves.toBeDefined();
+
+            // Inserindo sem nenhuma informação
+            await expect(News.create({})).rejects.toThrow();
+
+            // Inserindo sem título
+            await expect(News.create({
+                userId: user.id,
+                subject: "News Subject 01",
+                content: "News Content 01",
                 image: "https://picsum.photos/1920/350?random=1",
                 publishedAt: "2022-08-31T08:00:00"
-            },
-            {
-                title: "News 03",
-                subject: "News Subject 03",
-                content: "News Content 03",
-                image: "https://picsum.photos/1920/350?random=1",
-                publishedAt: "2022-08-31T08:00:00"
-            }
-        ]);
+            })).rejects.toThrow();
 
-        const news = await News.findAll();
-
-        expect(news.length).toBe(3);
-        expect(news[0].title).toBe("News 01");
-    });
-
-    test("Insert", async () => {  
-        // Inserindo com todos os dados      
-        const news = await News.create({
-            title: "News 01",
-            subject: "News Subject 01",
-            content: "News Content 01",
-            image: "https://picsum.photos/1920/350?random=1",
-            publishedAt: "2022-08-31T08:00:00"
         });
 
-        expect(news).toBeDefined();
-        expect(news).not.toBeNull();
-        expect(news.id).toBeDefined();
-        expect(news.title).toBe("News 01");
+        test("Update", async () => {
+            const news = await News.create({
+                userId: user.id,
+                title: "News 01",
+                subject: "News Subject 01",
+                content: "News Content 01",
+            });
 
-        // Inserindo com o mínimo de dados
-        await expect(News.create({
-            title: "News 01",
-            subject: "News Subject 01",
-            content: "News Content 01",
-        })).resolves.toBeDefined();
+            news.title = "Title updated";
+            await expect(news.save()).resolves.toBeTruthy();
 
-        // Inserindo com os dados brancos
-        await expect(News.create({
-            title: "",
-            subject: "",
-            content: "",
-        })).rejects.toThrow();
+            const news2 = await News.findOne({
+                where: {
+                    id: news.id
+                }
+            });
 
-        await expect(News.create({
-            title: "",
-            subject: "xxx",
-            content: "xxx",
-        })).rejects.toThrow();
+            expect(news2.title).toBe("Title updated");
 
-        await expect(News.create({
-            title: "xxx",
-            subject: "",
-            content: "xxx",
-        })).rejects.toThrow();
-
-        await expect(News.create({
-            title: "xxx",
-            subject: "xxx",
-            content: "",
-        })).rejects.toThrow();
-
-        // Inserindo sem data de publicação
-        await expect(News.create({
-            title: "News 01",
-            subject: "News Subject 01",
-            content: "News Content 01",
-            image: "https://picsum.photos/1920/350?random=1",
-        })).resolves.toBeDefined();
-
-        // Inserindo sem nenhuma informação
-        await expect(News.create({})).rejects.toThrow();
-
-        // Inserindo sem título
-        await expect(News.create({
-            subject: "News Subject 01",
-            content: "News Content 01",
-            image: "https://picsum.photos/1920/350?random=1",
-            publishedAt: "2022-08-31T08:00:00"
-        })).rejects.toThrow();
-
-    });
-
-    test("Update", async () => {
-        const news = await News.create({
-            title: "News 01",
-            subject: "News Subject 01",
-            content: "News Content 01",
         });
 
-        news.title = "Title updated";
-        await expect(news.save()).resolves.toBeTruthy();
+        test("Delete", async () => {
+            const news = await News.create({
+                userId: user.id,
+                title: "News 01",
+                subject: "News Subject 01",
+                content: "News Content 01",
+            });
 
-        const news2 = await News.findOne({
-            where: {
-                id: news.id
-            }
+            await expect(news.destroy()).resolves.toBeTruthy();
         });
-
-        expect(news2.title).toBe("Title updated");
-
     });
 
-    test("Delete", async () => {
-        const news = await News.create({
-            title: "News 01",
-            subject: "News Subject 01",
-            content: "News Content 01",
+    describe("Category", () => {
+
+        test("List", async () => {
+            await Category.bulkCreate([
+                {
+                    name: "Expreme sai sangue"
+                },
+                {
+                    name: "Moda"
+                },
+                {
+                    name: "Cultura"
+                },
+                {
+                    name: "Regionais"
+                }
+            ])
+
+            const categories = await Category.findAll();
+
+            expect(categories.length).toBe(4);
+        })
+
+        test("Insert", async () => {
+
+            const category = await Category.create({
+                name: "Classificados"
+            });
+
+            expect(category.id).toBeDefined();
+
+            await expect(Category.create({ name: "Classificados" })).rejects.toThrow();
+
+            await expect(Category.create({})).rejects.toThrow();
+
+            await expect(Category.create({
+                name: ""
+            })).rejects.toThrow();
+
+            await expect(Category.create({
+                name: "    "
+            })).rejects.toThrow();
         });
 
-        await expect(news.destroy()).resolves.toBeTruthy();
+        test("Update", async () => {
+            const category = await Category.create({
+                name: "Esportes"
+            });
+
+            category.name = "Jogos";
+
+            await expect(category.save()).resolves.toBeDefined();
+
+            const category2 = await Category.findOne({
+                where: {
+                    id: category.id
+                }
+            });
+
+            expect(category2.name).toBe(category.name);
+        });
+
+        test("Delete", async () => {
+            const category = await Category.create({
+                name: "Esportes"
+            });
+
+            await expect(category.destroy()).resolves.toBeTruthy();
+            
+        })
     });
+
 });
 
